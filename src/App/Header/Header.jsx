4c343f8ccdc0ea8a48df336eb959e034c7f1f95a
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-08-24 17:25:08 +0300

    test of Less features

--- src/App/Header/Header.jsx
+++ src/App/Header/Header.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./styles.less";
 
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="menu">
+        <div className="menu-item">a</div>
+        <div className="menu-item">b</div>
+        <div className="menu-item">c</div>
+        <div className="menu-item">d</div>
+        <div className="menu-item">e</div>
+        <div className="menu-item">f</div>
+        <div className="menu-item">g</div>
+        <div className="menu-item">h</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
