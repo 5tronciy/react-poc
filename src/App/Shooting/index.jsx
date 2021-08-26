c0427e43086f834ae47364a23e2d1d2a0f3d0f9f
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-08-26 16:05:35 +0300

    use YardTools

--- src/App/Shooting/index.jsx
+++ src/App/Shooting/index.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 import { Table, Icon } from "semantic-ui-react";
+import { YardTools, IFAAGroup } from "../../utils/YardTools";
 
 const initial = {
   editable: false,
   data: [
-    { distance: "9", correction: 0 },
-    { distance: "14", correction: 0 },
-    { distance: "18", correction: 0 },
-    { distance: "23", correction: 0 },
-    { distance: "27", correction: 0 },
-    { distance: "32", correction: 0 },
-    { distance: "37", correction: 0 },
-    { distance: "41", correction: 0 },
-    { distance: "46", correction: 0 },
-    { distance: "50", correction: 0 },
-    { distance: "55", correction: 0 },
-    { distance: "59", correction: 0 },
+    { distance: "10", correction: 0, tools: new YardTools(10) },
+    { distance: "15", correction: 0, tools: new YardTools(15) },
+    { distance: "20", correction: 0, tools: new YardTools(20) },
+    { distance: "25", correction: 0, tools: new YardTools(25) },
+    { distance: "30", correction: 0, tools: new YardTools(30) },
+    { distance: "35", correction: 0, tools: new YardTools(35) },
+    { distance: "40", correction: 0, tools: new YardTools(40) },
+    { distance: "45", correction: 0, tools: new YardTools(45) },
+    { distance: "50", correction: 0, tools: new YardTools(50) },
+    { distance: "55", correction: 0, tools: new YardTools(55) },
+    { distance: "60", correction: 0, tools: new YardTools(60) },
+    { distance: "65", correction: 0, tools: new YardTools(65) },
   ],
 };
+
+const yardTools = new YardTools(7);
 
 const Shooting = () => {
   const [edit, setEdit] = useState(initial.editable);
@@ -46,8 +49,8 @@ const Shooting = () => {
               {item.distance}
               {edit && <Icon name="plus circle" />}
             </Table.Cell>
-            <Table.Cell>{Math.round(item.distance / 1.094)}</Table.Cell>
-            <Table.Cell></Table.Cell>
+            <Table.Cell>{item.tools.toMeter()}</Table.Cell>
+            <Table.Cell>{item.tools.toGroup(IFAAGroup.Adult)}</Table.Cell>
             <Table.Cell>
               {edit && <Icon name="minus circle" />}
               {item.correction}
