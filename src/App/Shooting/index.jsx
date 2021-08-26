ca53a0fcb128afc1e0319440b2787b8b4ff08b1c
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-08-26 16:48:49 +0300

    add handlers for +/-

--- src/App/Shooting/index.jsx
+++ src/App/Shooting/index.jsx
@@ -1,28 +1,47 @@
 import React, { useState } from "react";
 import { Table, Icon } from "semantic-ui-react";
+import produce from "immer";
 import { YardTools, IFAAGroup } from "../../utils/YardTools";
 
 const initial = {
   editable: false,
   data: [
-    { distance: "10", correction: 0, tools: new YardTools(10) },
-    { distance: "15", correction: 0, tools: new YardTools(15) },
-    { distance: "20", correction: 0, tools: new YardTools(20) },
-    { distance: "25", correction: 0, tools: new YardTools(25) },
-    { distance: "30", correction: 0, tools: new YardTools(30) },
-    { distance: "35", correction: 0, tools: new YardTools(35) },
-    { distance: "40", correction: 0, tools: new YardTools(40) },
-    { distance: "45", correction: 0, tools: new YardTools(45) },
-    { distance: "50", correction: 0, tools: new YardTools(50) },
-    { distance: "55", correction: 0, tools: new YardTools(55) },
-    { distance: "60", correction: 0, tools: new YardTools(60) },
-    { distance: "65", correction: 0, tools: new YardTools(65) },
+    { id: 1, distance: 10, correction: 0, tools: new YardTools(10) },
+    { id: 2, distance: 15, correction: 0, tools: new YardTools(15) },
+    { id: 3, distance: 20, correction: 0, tools: new YardTools(20) },
+    { id: 4, distance: 25, correction: 0, tools: new YardTools(25) },
+    { id: 5, distance: 30, correction: 0, tools: new YardTools(30) },
+    { id: 6, distance: 35, correction: 0, tools: new YardTools(35) },
+    { id: 7, distance: 40, correction: 0, tools: new YardTools(40) },
+    { id: 8, distance: 45, correction: 0, tools: new YardTools(45) },
+    { id: 9, distance: 50, correction: 0, tools: new YardTools(50) },
+    { id: 10, distance: 55, correction: 0, tools: new YardTools(55) },
+    { id: 11, distance: 60, correction: 0, tools: new YardTools(60) },
+    { id: 12, distance: 65, correction: 0, tools: new YardTools(65) },
   ],
 };
 
-const yardTools = new YardTools(7);
-
 const Shooting = () => {
+  const [data, setData] = useState(initial.data);
+
+  const distanceHandler = (id, operator) => {
+    setData(
+      produce((draft) => {
+        const item = draft.find((item) => item.id === id);
+        item.distance += Number(operator);
+      })
+    );
+  };
+
+  const correctionHandler = (id, operator) => {
+    setData(
+      produce((draft) => {
+        const item = draft.find((item) => item.id === id);
+        item.correction += Number(operator);
+      })
+    );
+  };
+
   const [edit, setEdit] = useState(initial.editable);
   const onEdit = () => setEdit(!edit);
   return (
@@ -42,19 +61,39 @@ const Shooting = () => {
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {initial.data.map((item) => (
-          <Table.Row key={item.distance.toString() + item.correction}>
+        {data.map((item) => (
+          <Table.Row key={item.id}>
             <Table.Cell>
-              {edit && <Icon name="minus circle" />}
+              {edit && (
+                <Icon
+                  name="minus circle"
+                  onClick={() => distanceHandler(item.id, -1)}
+                />
+              )}
               {item.distance}
-              {edit && <Icon name="plus circle" />}
+              {edit && (
+                <Icon
+                  name="plus circle"
+                  onClick={() => distanceHandler(item.id, 1)}
+                />
+              )}
             </Table.Cell>
             <Table.Cell>{item.tools.toMeter()}</Table.Cell>
             <Table.Cell>{item.tools.toGroup(IFAAGroup.Adult)}</Table.Cell>
             <Table.Cell>
-              {edit && <Icon name="minus circle" />}
+              {edit && (
+                <Icon
+                  name="minus circle"
+                  onClick={() => correctionHandler(item.id, -0.5)}
+                />
+              )}
               {item.correction}
-              {edit && <Icon name="plus circle" />}
+              {edit && (
+                <Icon
+                  name="plus circle"
+                  onClick={() => correctionHandler(item.id, 0.5)}
+                />
+              )}
             </Table.Cell>
             <Table.Cell></Table.Cell>
           </Table.Row>
