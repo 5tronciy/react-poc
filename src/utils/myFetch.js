12b6511609e3c7aea3d7d5e1067dcfa55945dd9c
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-09-07 13:39:24 +0300

    check filter and sort exist

--- src/utils/myFetch.js
+++ src/utils/myFetch.js
@@ -1,8 +1,12 @@
 export const myFetch = async (endpoint, { filter, include, order }, signal) => {
     const response = await fetch(
-        `rest/${endpoint}?cayenneExp=${encodeURIComponent(
-            JSON.stringify(filter)
-        )}${(include || []).map((i) => `&include=${i}`).join()}&sort=${order}`,
+        `rest/${endpoint}?cayenneExp=${
+            filter !== undefined
+                ? encodeURIComponent(JSON.stringify(filter))
+                : ""
+        }${(include || []).map((i) => `&include=${i}`).join()}&sort=${
+            order !== undefined ? order : ""
+        }`,
         { signal }
     );
     return response.json();
