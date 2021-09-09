06d0443e6548470c935dafcf733899ea0089c284
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-09-09 17:22:51 +0300

    add errorHandling&validation to inputs

--- src/App/NHL/PlayersList/PlayerCrudModal/PlayerCrudModal.jsx
+++ src/App/NHL/PlayersList/PlayerCrudModal/PlayerCrudModal.jsx
@@ -8,6 +8,17 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
     const [player, setPlayer] = useState({});
     const [edit, setEdit] = useState(false);
     const [difference, setDifference] = useState({});
+    const [formValid, setFormValid] = useState(false);
+    const [errors, setErrors] = useState({
+        firstName: "",
+        middleName: "",
+        lastName: "",
+    });
+    const [dirties, setDirties] = useState({
+        firstName: false,
+        middleName: false,
+        lastName: false,
+    });
 
     const options = positions.map((position) => {
         return { key: position.id, value: position.name, text: position.name };
@@ -20,6 +31,14 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
         setPlayer(data.data[0]);
     }, [value]);
 
+    useEffect(() => {
+        if (errors.firstName || errors.middleName || errors.lastName) {
+            setFormValid(false);
+        } else {
+            setFormValid(true);
+        }
+    }, [errors.firstName, errors.middleName, errors.lastName]);
+
     const onSubmit = (e) => {
         fetch(`rest/player/${value}`, {
             method: "PUT",
@@ -29,22 +48,84 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
             body: JSON.stringify(difference),
         });
         setEdit(false);
+        setDirties({
+            firstName: false,
+            middleName: false,
+            lastName: false,
+        });
+        setDifference({});
+        setErrors({
+            firstName: "",
+            middleName: "",
+            lastName: "",
+        });
         e.preventDefault();
     };
 
-    const onChangeHandler = (e) => {
+    const inputHandler = (e) => {
         setDifference({ ...difference, [e.target.name]: e.target.value });
+
         setPlayer({
             ...player,
             [e.target.name]: e.target.value,
         });
+
+        const re = /^[A-Z]{1}[a-zA-Z]{1,30}$/gm;
+
+        if (
+            (e.target.value && re.test(String(e.target.value))) ||
+            (!e.target.value && e.target.name === "middleName")
+        ) {
+            setErrors({ ...errors, [e.target.name]: "" });
+        } else {
+            if (
+                !e.target.value &&
+                (e.target.name === "firstName" || e.target.name === "lastName")
+            ) {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: "The field can not be empty!",
+                });
+            } else {
+                setErrors({ ...errors, [e.target.name]: "Invalid value!" });
+            }
+        }
     };
 
     const selectHandler = (e) => {
         setDifference({ ...difference, position: e.target.innerText });
+
         setPlayer({
             ...player,
             position: e.target.innerText,
+        });
+    };
+
+    const focusHandler = (e) => {
+        setDirties({ ...dirties, [e.target.name]: false });
+    };
+
+    const blurHandler = (e) => {
+        setDirties({ ...dirties, [e.target.name]: true });
+    };
+
+    const closeHandler = async () => {
+        onClose(false);
+        const data = await myFetch(`player/${value}`, {
+            include: ["team"],
+        });
+        setPlayer(data.data[0]);
+        setEdit(false);
+        setDirties({
+            firstName: false,
+            middleName: false,
+            lastName: false,
+        });
+        setDifference({});
+        setErrors({
+            firstName: "",
+            middleName: "",
+            lastName: "",
         });
     };
 
@@ -68,8 +149,18 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
                             placeholder="First name"
                             type="text"
                             value={player.firstName}
-                            onChange={onChangeHandler}
+                            onChange={inputHandler}
                             readOnly={!edit}
+                            onBlur={blurHandler}
+                            onFocus={focusHandler}
+                            error={
+                                edit &&
+                                dirties.firstName &&
+                                !!errors.firstName && {
+                                    content: errors.firstName,
+                                    pointing: "below",
+                                }
+                            }
                         />
                         {(player.middleName || edit) && (
                             <Form.Input
@@ -78,8 +169,18 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
                                 placeholder="Middle name"
                                 type="text"
                                 value={player.middleName}
-                                onChange={onChangeHandler}
+                                onChange={inputHandler}
                                 readOnly={!edit}
+                                onBlur={blurHandler}
+                                onFocus={focusHandler}
+                                error={
+                                    edit &&
+                                    dirties.middleName &&
+                                    !!errors.middleName && {
+                                        content: errors.middleName,
+                                        pointing: "below",
+                                    }
+                                }
                             />
                         )}
                         <Form.Input
@@ -88,8 +189,18 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
                             placeholder="Last name"
                             type="text"
                             value={player.lastName}
-                            onChange={onChangeHandler}
+                            onChange={inputHandler}
                             readOnly={!edit}
+                            onBlur={blurHandler}
+                            onFocus={focusHandler}
+                            error={
+                                edit &&
+                                dirties.lastName &&
+                                !!errors.lastName && {
+                                    content: errors.lastName,
+                                    pointing: "below",
+                                }
+                            }
                         />
                         {(player.birthDate || edit) && (
                             <Form.Input
@@ -98,7 +209,7 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
                                 placeholder="Birth date"
                                 type="date"
                                 value={player.birthDate}
-                                onChange={onChangeHandler}
+                                onChange={inputHandler}
                                 readOnly={!edit}
                             />
                         )}
@@ -138,11 +249,15 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
             </Modal.Content>
             <Modal.Actions>
                 {edit ? (
-                    <Input type="submit" form="player-form" value="Save" />
+                    <Input
+                        disabled={!formValid}
+                        type="submit"
+                        form="player-form"
+                        value="Save"
+                    />
                 ) : (
                     <Button
                         color="grey"
-                        onClick={() => onClose(false)}
                         labelPosition="right"
                         icon
                         onClick={() => setEdit(true)}
@@ -155,7 +270,7 @@ export const PlayerCrudModal = ({ open, onClose, value }) => {
                     content="Close"
                     labelPosition="right"
                     icon="checkmark"
-                    onClick={() => onClose(false)}
+                    onClick={closeHandler}
                     positive
                 />
             </Modal.Actions>
